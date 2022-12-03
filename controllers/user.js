9b646bf7a1395b8bd65d1dd1fe2118928c53f11b
Sakinah Freeman <freemansakinah@gmail.com>
2022-12-03 12:34:10 -0500

    creates routes for user

---
+++ controllers/user.js
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////
+//////// Import Dependencies
+///////////////////////////////////////////////
+const express = require("express")
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+
+//////////////////////////////////////////////
+//////// Create Route
+///////////////////////////////////////////////
+const router = express.Router()
+
+//////////////////////////////////////////////
+//////// Routes
+///////////////////////////////////////////////
+
+// The Signup Routes (Get => form, post => submit form)
+router.get("/signup", (req, res) => {
+    res.render("user/signup.ejs")
+})
+
+router.post("/signup", (req, res) => {
+    res.send("signup")
+})
+
+// The login Routes (Get => form, post => submit form)
+router.get("/login", (req, res) => {
+    res.render("user/login.ejs")
+})
+
+router.post("/login", (req, res) => {
+    res.send("login")
+})
+
+//////////////////////////////////////////////
+//////// Export Router
+///////////////////////////////////////////////
+module.exports = router
